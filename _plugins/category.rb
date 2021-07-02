d8070de7f18331e2f302482e94416525e867f1df
Christoph Kappel <christoph@unexist.dev>
2021-07-02 13:04:20 +0200

    Updated category plugin

--- _plugins/category.rb
+++ _plugins/category.rb
@@ -1,35 +1,59 @@
 module Jekyll
-
-  class CategoryIndex < Page
-    def initialize(site, base, dir, category)
-      @site = site
-      @base = base
-      @dir = dir
-      @name = 'index.html'
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'category_page.html')
-      self.data['category'] = category
-      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
-      self.data['title'] = "#{category_title_prefix}#{category}"
+    module Helpers
+        def jekyll_category_slug(str)
+            str.to_s.replace_diacritics.downcase.gsub(/\s/, '-')
+        end
     end
+
+    class CategoryGenerator < Generator
+        include Helpers
+        safe true
+        attr_accessor :site
+        @types = [:page, :feed]
+
+        class << self; attr_accessor :types, :site; end
+
+        def generate(site)
+            self.class.site = self.site = site
+
+            site.categories.keys.each do |category, posts|
+                new_category(category, posts)
+            end
+        end
+
+        def new_category(category, posts)
+            self.class.types.each { |type|
+                if layout = site.config["category_#{type}_layout"]
+                    data = { 'layout' => layout, 'posts' => posts.sort.reverse!, 'category' => category }
+                    data.merge!(site.config["category_#{type}_data"] || {})
+
+                    name = yield data if block_given?
+                    name ||= category
+                    name = jekyll_category_slug(name)
+
+                    category_dir = site.config["category_#{type}_dir"]
+                    category_dir = File.joincategory_dir, (pretty? ? name : ''))
+
+                    page_name = "#{pretty? ? 'index' : name}#{site.layouts[data['layout']].ext}"
+
+                    site.pages << CategoryPage.new(
+                        site, site.source, category_dir, page_name, data
+                    )
+                end
+            }
+        end
   end
 
-  class CategoryGenerator < Generator
-    safe true
+  class CategoryPage < Page
+    def initialize(site, base, dir, name, data = {})
+        self.content = data.delete('content') || ''
+        self.data    = data
 
-    def generate(site)
-      if site.layouts.key? 'category_index'
-        dir = site.config['category_dir'] || 'categories'
-        site.categories.keys.each do |category|
-          write_category_index(site, File.join(dir, category), category)
-        end
-      end
+        super(site, base, dir[-1, 1] == '/' ? dir : '/' + dir, name)
     end
 
-    def write_category_index(site, dir, category)
-      index = CategoryIndex.new(site, site.source, dir, category)
-      index.render(site.layouts, site.site_payload)
-      index.write(site.dest)
+    def read_yaml(*)
+      # Do nothing
     end
   end
 end
