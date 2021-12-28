9770f76dcf0c9e9d6e93363272036e3ae2447ce6
Christoph Kappel <christoph@unexist.dev>
2021-12-28 22:47:04 +0100

    Updated posts

--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@
 
 DATE="`date +'%Y-%m-%d'`"
 
-ls _drafts | fzf --tac --no-sort | xargs -i -r mv _drafts/{} _posts/${DATE}-{}
+ls _drafts | fzf --tac --no-sort | xargs -i -r hg mv _drafts/{} _posts/${DATE}-{}
 
 sed -i -e "s#%%%DATE%%%#${DATE}#" _posts/${DATE}-*.md
