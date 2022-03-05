9a113c69fba600a96c22423719513061892f57d3
Christoph Kappel <christoph@unexist.dev>
2022-03-05 16:59:42 +0100

    Updated link collector

--- collect.sh
+++ collect.sh
@@ -5,11 +5,16 @@ IDX=1
 while read LINK; do
     LINKNAME=`echo $LINK | cut -d "]" -f 1 | tr -d "[]"`
 
-    sed -i -e "s#\[${LINKNAME}\]\[[0-9]*\]#\[$LINKNAME\]\[$IDX\]#g" $FILE
+    # Respect case
+    LINKNAME_UP=`echo ${LINKNAME:0:1} | tr '[:lower:]' '[:upper:]'`${LINKNAME:1}
+    LINKNAME_DOWN=`echo ${LINKNAME:0:1} | tr '[:upper:]' '[:lower:]'`${LINKNAME:1}
+
+    sed -i -e "s#\[${LINKNAME_UP}\]\[[0-9]*\]#\[$LINKNAME_UP\]\[$IDX\]#g" $FILE
+    sed -i -e "s#\[${LINKNAME_DOWN}\]\[[0-9]*\]#\[$LINKNAME_DOWN\]\[$IDX\]#g" $FILE
 
     LINKNAME=`echo $LINKNAME | tr -d ' '`
 
-    echo "[$IDX]: $LINKNAME" >> $FILE
+    echo "[$IDX]: <$LINKNAME>" >> $FILE
 
     IDX=`expr $IDX + 1`
-done < <(\grep -oh "\[[^[]*\]\[[0-9]*\]" $FILE | sort -u)
+done < <(\grep -oh "\[[^[]*\]\[[0-9]*\]" $FILE | sort -uf)
