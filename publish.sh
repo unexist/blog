51405352ba3d55c324181c1f43ac3a7d53baaba5
Christoph Kappel <christoph@unexist.dev>
2022-07-04 13:18:23 +0200

    Added last_updated to scripts

--- publish.sh
+++ publish.sh
@@ -4,4 +4,4 @@ DATE="`date +'%Y-%m-%d'`"
 
 ls _drafts | fzf --tac --no-sort | xargs -i -r hg mv _drafts/{} _posts/${DATE}-{}
 
-sed -i -e "s#%%%DATE%%%#${DATE}#" _posts/${DATE}-*.md
+sed -i -e "s#%%%DATE%%%#${DATE}#g" _posts/${DATE}-*.md
