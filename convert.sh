6cf5cdaa50156eb96364ae13fdd1114a8ef9299c
Christoph Kappel <christoph@unexist.dev>
2022-07-28 09:23:55 +0200

    Added conversion script

---
+++ convert.sh
@@ -0,0 +1,24 @@
+OLDFILE=$1
+NEWFILE=${1/.md/.adoc}
+
+hg mv $OLDFILE $NEWFILE
+
+# Convert links
+sed -i -e "s/\[\([^]]*\)\]\[\([^]]*\)\]/{\2}[\1]/g" $NEWFILE
+
+# Convert marker
+sed -i -e "s/^\[\([0-9]*\)\]:/:\1:/g" $NEWFILE
+
+# Convert source
+for KIND in java shell hcl log json yaml xml; do
+    sed -i -e "s/^\`\`\`$KIND/[source,$KIND]\n----/g" $NEWFILE
+done
+
+sed -i -e "s/^\`\`\`$/----/g" $NEWFILE
+
+# Convert callouts
+sed -i -e "s/\*\*<\([0-9]+\)>\*\*/<\1>/g" $NEWFILE
+
+# Convert headers
+sed -i -e "s/##/==/g" $NEWFILE
+sed -i -e "s/==#/===/g" $NEWFILE
